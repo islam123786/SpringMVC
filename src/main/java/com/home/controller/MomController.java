e5c05f68795338f6a240d5e236c2306438fb4698
Mohammed Siddiqui <islam123786@gmail.com>
2022-08-16 23:50:30 +0530

    Added jsp file as well as ViewResolver bean in XML file.

--- src/main/java/com/home/controller/MomController.java
+++ src/main/java/com/home/controller/MomController.java
@@ -2,15 +2,13 @@ package com.home.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 @Controller
 public class MomController {
 	
-	@ResponseBody
+	
 	@RequestMapping("/sugar")
 	public String giveSugar() {
-		return "OK.. Take Sugar";
+		return "main";
 	}
 
 }
